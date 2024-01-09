a43f95c26b456afc32f7729eb4d539a8bfa677c6
unknown <isaiaspadros2@gmail.com>
2024-01-09 12:08:51 -0300

    Borrado de comentarios pt2

--- main_video.py
+++ main_video.py
@@ -1,17 +1,17 @@
 import cv2
 from simple_facerec import SimpleFacerec
 
-# Codificar las caras desde una carpeta
+
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
 
-# Cargar cámara
+
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
-    # Detectar caras
+    
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
--- simple_facerec.py
+++ simple_facerec.py
@@ -8,7 +8,7 @@ class SimpleFacerec:
     def __init__(self):
         self.known_face_encodings = []
         self.known_face_names = []
-        # Redimensionar el frame para una mayor velocidad
+        
         self.frame_resizing = 0.25
 
     def load_encoding_images(self, images_path):
@@ -17,49 +17,48 @@ class SimpleFacerec:
         :param images_path:
         :return:
         """
-        # Cargar imágenes
+        
         images_path = glob.glob(os.path.join(images_path, "*.*"))
 
         print("{} imágenes de codificación encontradas.".format(len(images_path)))
 
-        # Almacenar la codificación y los nombres de las imágenes
+        
         for img_path in images_path:
             img = cv2.imread(img_path)
             rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # Obtener solo el nombre del archivo de la ruta inicial del archivo.
+            
             basename = os.path.basename(img_path)
             (filename, ext) = os.path.splitext(basename)
-            # Obtener la codificación
+            
             img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-            # Almacenar el nombre del archivo y la codificación del archivo
+           
             self.known_face_encodings.append(img_encoding)
             self.known_face_names.append(filename)
         print("Imágenes de codificación cargadas")
 
     def detect_known_faces(self, frame):
         small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
-        # Encontrar todas las caras y codificaciones de caras en el frame actual de video
-        # Convertir la imagen de color BGR (que usa OpenCV) a color RGB (que usa face_recognition)
+        
         rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
-            # Verificar si la cara es una coincidencia con la(s) cara(s) conocida(s)
+            
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
             name = "Desconocido"
 
-            # O en su lugar, usar la cara conocida con la distancia más pequeña a la nueva cara
+           
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
             face_names.append(name)
 
-        # Convertir a matriz numpy para ajustar las coordenadas con el redimensionado del frame rápidamente
+        
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resizing
         return face_locations.astype(int), face_names
